30ad51c0017e19a29818e89d7faa3c2a66ebad98
Ed <edcsalter@gmail.com>
2023-01-05 16:12:46 -0500

    Confirmed Benchtimer viability

--- Big 0.playground/Contents.swift
+++ Big 0.playground/Contents.swift
@@ -1,3 +1,9 @@
 import UIKit
 
-var greeting = "Hello, playground"
+var numbers = Array(0..<1000)
+let sortTime = Benchtimer.measureBlock {
+    numbers.sort()
+}
+
+print("Sorting took \(sortTime.formattedTime)") // Sorting took 148µs
+
